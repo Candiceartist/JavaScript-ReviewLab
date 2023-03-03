2843e7aff5a03cb31b1217aa4360d0d43bb07aca
Candice & <>
2023-03-03 03:26:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+// // How do we assign a value to a variable ?
+// //   Use assignment operator "="
+
+// let assignment = "value"
+// assignment = "changeValue"
+// console.log(assignment)
+// // How do we change the value of a variable ?
+
+// let oldVar = "Jordan"
+// let newVar = "manara"
+
+// let collabVar - oldvar = newvar
+// //   write value to the right of assignment operator after operator in parenthesis()
+// // How do we assign an existing variable to a new variable ? use it's name
+// //   Remind me, what are declare, assign, and define ?
+// //   writing name of variable, using assignment operator and setting a value after operator.
+// //     What is pseudocoding and why should you do it ? simplified coding languange that describes what you need your functions to do.plan your code ahead of time. a map understood by programmers of all types.
+// //       What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it? 60/40
+
+// let firstVariable = ("Hello World") => {
+//   helloWorld = 0
+// }
+// let secondVariable = (firstVariable)
+// let yourNAme = (name) => {
+//   name = "Candice"
+// }
+
+// // console.log('${firstVAriable}, my name is ${yourName}')
+let firstVar - "first"
+firstVar = "Helow World"
+firstVar = 1
+let secvat = firstVar
+secondVar = "any string"
+
+// console.log(firstVar)
+
+let name - "Jason"
+let greeting is "Hi my name is ${name}"
+// console.log(greeting)
+// const person = (age) => {
+//   let age = 35
+// //   return age
+// // }
+
+if age > 16 {
+// //   console.log("Here are the keys")
+// // }
+
+for (x >= 0; x < 10; x++) {
+  if (x != 0)
+//     console.log(x)
+// } else {
+//   console.log('Finished')
+// }
+
+// booleans 
+// make statements true or false 
+
+let animal = "cow" || "sumElse"
+if (animal === "cow") {
+//   console.log("Moo")
+// } else {
+//   console.log("youre not a cow!")
+// }
+
+// let age=33
+
+// if(age >= 16){
+//   console.log("here are the keys")
+// } else {
+//   console.log("you can walk")
+// }
+
+for (let i = 0; i <= 10; i++) {
+  // console.log(i, "current number")
+}
+
+for (let i = 12; i <= 4000; i += 3) {
+  // console.log(i, "current")
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    // console.log(i, "is even")
+  }
+}
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 5 === 0) {
+    // console.log(i, "Hi Five")
+  } else {
+    // console.log(i, "skip")
+  }
+}
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    // console.log(i, "Threes a crowd and Fizz Buzz")
+  } else if (i % 5 === 0) {
+    // console.log(i, "High Five")
+  } else if (i % 3 === 0) {
+    // console.log(i, "Threes a crowd")
+  } else {
+    //   console.log(i, "skip")
+    // }
+  }
+  let accountBalance = 25
+  for (let i = 1; i <= 100; i++) {
+    accountbalance += i
+    // } console.log(accountBalance * 2)                 
+    let quote = [
+      'YadaYada',
+      'wordsBond',
+      'BadaBing'
+    ]
+
+  constant randomThings = [1, 10 "hello", true]
+
+    randomThings[0]
+    randomThings[2] = "World"  console.log(randomThings)
